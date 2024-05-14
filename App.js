84441a9d18688f6abfb6596c65012103f9239331
Ritik Gupta <ritikgupta2602@gmail.com>
2024-05-14 21:43:28 +0530

    front end

--- App.js
+++ App.js
@@ -1,9 +1,57 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Heading= ()=> (
-    <h1>React</h1>
-);
+const Header = () => {
+    return(
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://marketplace.canva.com/EAFaFUz4aKo/2/0/1600w/canva-yellow-abstract-cooking-fire-free-logo-JmYWTjUsE-Q.jpg"/>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Contact us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
 
+const RestaurantCard = (props) => {
+    return(
+        <div className="res-card">
+            <img className="res-logo" alt="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/8144bf37947c161953f656308d1bfc30"/>
+            <div className="info">
+            <h3>{props.resname}</h3>
+            <h4>{props.cuisine}</h4>
+            <h4>4.1 stars</h4>
+            <h4>34 mins</h4>
+            </div>
+        </div>
+    )
+}
+
+const Body = () => {
+    return(
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                <RestaurantCard resname="Kadak Singh" cuisine="North Indian, Paneer"/>
+                <RestaurantCard resname="KFC" cuisine="Burger, Fast Food"/>
+            </div>
+        </div>
+    )
+}
+
+const AppLayout = () => {
+    return(
+        <div className="app">
+            <Header/>
+            <Body/>
+        </div>
+    )
+}
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Heading />);
+root.render(<AppLayout />);
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Food Ordering App's Clone basically the front end
+
+# Components
+- Header
+    - Login
+    - Nav Items
+- Body
+    - Search
+    - Restaurant Card
+        - Img
+        - Name of res, star rating, cuisine, delivery time
+- Footer
+    - Copyright
+    - Link 
+    - Address
+    - Contact
